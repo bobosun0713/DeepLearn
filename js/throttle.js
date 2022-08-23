de61fb8d7459c6f517e787080a48514a7876056f
bobosun0713 <chiabo.sun@gmail.com>
2022-08-23 22:57:49 +0800

    feat: add throttle

---
+++ js/throttle.js
@@ -0,0 +1,25 @@
+/**
+ * @description
+ * 節流
+ *
+ * @param {function} fn 傳入要被執行的函式
+ * @param {number} delay 延遲幾毫後觸發 預設500ms
+ *
+ * @example
+ * ```js
+ * function callFn(msg){ console.log('trigger ' + msg)}
+ * const throttleFn = throttle(callFn,300)
+ * ```
+ */
+
+function throttle(fn, delay = 500) {
+  let prevTime = Date.now()
+  return function () {
+    const args = arguments
+    let currentTime = Date.now()
+    if (currentTime - prevTime >= delay) {
+      fn.apply(this, args)
+      prevTime = currentTime
+    }
+  }
+}
