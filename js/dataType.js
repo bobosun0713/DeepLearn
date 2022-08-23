abeb0889383f4846a972423cd30334232ce0ab56
bobosun0713 <chiabo.sun@gmail.com>
2022-08-23 22:18:10 +0800

    feat: add dataType

---
+++ js/dataType.js
@@ -0,0 +1,25 @@
+/**
+ * @description
+ * 型別檢查，當傳入兩個參數時，為型別比對
+ *
+ * @param {function} target 隨意型別
+ * @param {string} type 需要對比的型別，
+ *
+ * @example
+ * ```js
+ * const a = []
+ * dataType(a)
+ * // 回傳 => 'array'
+ *
+ * dataType(a,'array')
+ * // 回傳 => true
+ * ```
+ */
+
+function dataType(target, type) {
+  const checkType = Object.prototype.toString
+    .call(target)
+    .replace(/\[object (\w+)\]/, '$1')
+    .toLowerCase()
+  return type ? type === checkType : checkType
+}
