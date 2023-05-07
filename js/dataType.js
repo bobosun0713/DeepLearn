77d7adc91ffeb2849fa3a6568f6f6bdb3272606b
bobosun0713 <chiabo.sun@gmail.com>
2023-05-07 21:51:32 +0800

    docs: update the doc in dataType.js

--- js/dataType.js
+++ js/dataType.js
@@ -1,9 +1,8 @@
 /**
- * @description
- * 型別檢查，當傳入兩個參數時，為型別比對
- *
+ * @description 型別檢查，當傳入兩個參數時，為型別比對
  * @param {function} target 隨意型別
  * @param {string} type 需要對比的型別，
+ * @return {boolean|string}
  *
  * @example
  * ```js
